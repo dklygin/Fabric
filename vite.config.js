26cde7ad87add34840ebfb94ce5a24acd8827225
dklygin <74540631+dklygin@users.noreply.github.com>
2026-09-17 14:20:47 +0500

    Update base path in Vite configuration
    Set base path for the project to '/Fabric/'

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Fabric/',  // ← замените на имя вашего репозитория
   server: {
     host: "0.0.0.0",
     port: 3000,
